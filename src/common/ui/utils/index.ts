e122185e387fe0816b5f30cec10f443a1556dbc8
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-04-21 11:24:32 +0400

    Test pre-commit hook

--- src/common/ui/utils/index.ts
+++ src/common/ui/utils/index.ts
@@ -1,1 +1,6 @@
-export * from "./classnames"
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
